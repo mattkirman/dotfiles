dfeeec7218d76731df23b8dbeb596cf44c876abc
Matt Kirman <matt@mattkirman.com>
2012-09-09 18:54:52 +0100

    Added a 'whoami' Tmux powerline script

--- tmux/powerline/status-left.sh
+++ tmux/powerline/status-left.sh
@@ -16,6 +16,15 @@ hostname[background]='colour148'
 hostname[separator]="${separator_right_bold}"
 register_segment 'hostname'
 
+# Whoami
+#typeset -A whoami
+#whoami=()
+#whoami[script]="${segments_path}/whoami.sh"
+#whoami[foreground]='colour250'
+#whoami[background]='colour238'
+#whoami[separator]="${separator_right_bold}"
+#register_segment 'whoami'
+
 # Print the status line in the order of registration above
 print_status_line_left
 
