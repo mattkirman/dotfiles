89b57d33c03ce44faab7eb4aed7c5e9865a93b47
Matt Kirman <matt@mattkirman.com>
2013-04-26 11:07:58 +0100

    Prefer unicode high voltage rather than emoji

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -24,5 +24,5 @@ RPROMPT='$(return_code)%{$fg[cyan]%}$(rbenv_version)%{$reset_color%}'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}:"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY=") %{$fg[yellow]%}⚡%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY=") %{$fg[yellow]%}\xE2\x9A\xA1\xEF\xB8\x8E%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_CLEAN=") "
