1854a8db5abb9a13c3d4fa6fc114682c61a68151
Matt Kirman <matt@mattkirman.com>
2016-02-15 21:42:18 +0000

    Added elapsed time for the previous command to the right prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,12 +12,38 @@ function return_code {
   echo "%(?..%{$fg[red]%}%? ↵ %{$reset_color%})"
 }
 
+function preexec {
+  (( $#_elapsed > 10 )) && set -A _elapsed $_elapsed[-10,-1]
+  typeset -ig _start=SECONDS
+}
+
+function precmd {
+  (( _start >= 0 )) && set -A _elapsed $_elapsed $(( SECONDS-_start ))
+   _start=-1
+ }
+
+function elapsed_time {
+  elapsed=$_elapsed[-1]
+
+  if [[ $elapsed -gt 0 ]]; then
+    (( minutes = $elapsed / 60 ))
+    (( seconds = $elapsed - (minutes * 60) ))
+
+    time="${seconds}s"
+    if [[ $minutes != "0" ]]; then
+      time="${minutes}m ${time}"
+    fi
+
+    echo "%{$fg[yellow]%}[$time]%{$reset_color%}"
+  fi
+}
+
 function timestamp {
   echo " %{$fg[cyan]%}$(date +"%H:%M:%S")%{$reset_color%}"
 }
 
 PROMPT='%{$fg[blue]%}%c%{$reset_color%} $(git_prompt_info)→ %{$reset_color%}'
-RPROMPT='$(return_code)%{$reset_color%}$(timestamp)'
+RPROMPT='$(return_code)%{$reset_color%}$(elapsed_time)$(timestamp)'
 
 if [[ $EUID -eq 0 ]]
 then
